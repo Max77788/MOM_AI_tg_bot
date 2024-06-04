ae7d691ffca2ed1434328d3623a4024f41c18978
Maxik <weee@mail.com>
2024-06-04 21:56:08 +0000

    Modified telegram.py and added wsgi.py

--- telegram.py
+++ telegram.py
@@ -1,7 +1,6 @@
 from pyrogram import Client, filters
 import os
 from dotenv import load_dotenv, find_dotenv
-from flask import Flask
 import threading
 
 load_dotenv(find_dotenv())
@@ -10,7 +9,6 @@ load_dotenv(find_dotenv())
 api_id = os.environ.get("TELEGRAM_API_ID")
 api_hash = os.environ.get("TELEGRAM_API_HASH")
 bot_token = os.environ.get("TELEGRAM_BOT_TOKEN")
-port = int(os.environ.get("PORT", 10000))  # Default to 10000 if PORT is not set
 
 # Initialize the Pyrogram client
 app_tg = Client("mom_ai_tg_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
@@ -22,23 +20,11 @@ async def get_chat_id(client, message):
     await message.reply('The code to insert:')
     await message.reply(f'{chat_id}')
 
-# Initialize Flask app
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'Hello, this is the Pyrogram bot server running!'
-
 # Function to run the Pyrogram bot
 def run_bot():
     app_tg.run()
 
-# Function to run the Flask server
-def run_flask():
-    app.run(host='0.0.0.0', port=port)
-
-# Run both Flask server and Pyrogram bot in separate threads
+# Run the Pyrogram bot in a separate thread
 if __name__ == "__main__":
     threading.Thread(target=run_bot).start()
-    threading.Thread(target=run_flask).start()
 
