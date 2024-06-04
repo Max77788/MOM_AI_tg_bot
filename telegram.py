f88e0c85c2a3b1a301597d525bf3590ed765ff5f
Maxik <weee@mail.com>
2024-06-04 20:49:46 +0000

    Modified the message

--- telegram.py
+++ telegram.py
@@ -13,7 +13,7 @@ app_tg = Client("mom_ai_tg_bot", api_id=api_id, api_hash=api_hash, bot_token=bot
 @app_tg.on_message(filters.command("start"))
 async def get_chat_id(client, message):
     chat_id = message.chat.id
-    await message.reply('Hi! Please insert the code provided below in your account Settings -> Notifications on mom-ai-restaurant.pro\n\nAfter the insertion MOM AI Restaurant Assistant will send the notifications in this chat upon the arrival of successfully paid orders!')
+    await message.reply('Hi! Please insert the code provided below on your account dashboard in the section Profile -> Notifications on mom-ai-restaurant.pro\n\nAfter the insertion MOM AI Restaurant Assistant will send the notifications in this chat upon the arrival of successfully paid orders!')
     await message.reply('The code to insert:')
     await message.reply(f'{chat_id}')
 app_tg.run()
